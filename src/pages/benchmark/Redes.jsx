a4d3cbf6b234fb9f2d6d693e11bc358d4b49d293
bisecompr <150809003+bisecompr@users.noreply.github.com>
2024-02-02 19:10:28 -0300

    Atualização Bench Redes.jsx
    Atualização do 4° Trimestre

--- src/pages/benchmark/Redes.jsx
+++ src/pages/benchmark/Redes.jsx
@@ -4,7 +4,7 @@ const Redes = () => {
   return (
     <div>
         
-        <iframe title="Campanhas Finalizadas 2023 - Redes Sociais" width="75%" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiZDViNTkzOGQtM2Q1Mi00MWQ1LWI0ZTEtN2JhOTRhYzUzZDVhIiwidCI6ImExMTIwMGVkLTNhYTctNDFhMy05M2UxLTcwYWU4ZmMxZWMxYSJ9&pageName=ReportSection6c811c3f74c96771caf5" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="Campanhas Finalizadas 2023 - Redes Sociais" width="75%" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiMWU1ZGQwZWUtOTU2MC00Zjg3LWI2ZjQtNGZlMjExNTY5ZjEyIiwidCI6IjFjYzNjNTA4LTAxYzctNDQ2MC1iZDJiLWFmZTk1ZTgwYjhhZiJ9&pageName=ReportSection6c811c3f74c96771caf5" frameborder="0" allowFullScreen="true"></iframe>
 
     </div>
   )
