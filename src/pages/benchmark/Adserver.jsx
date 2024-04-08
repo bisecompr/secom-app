2398ffeab8440bf3aad151ac5a48e0b76eb18728
bisecompr <150809003+bisecompr@users.noreply.github.com>
2024-04-08 15:32:36 -0300

    Update Adserver.jsx
    Atualização do dash de AdServer

--- src/pages/benchmark/Adserver.jsx
+++ src/pages/benchmark/Adserver.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Adserver = () => {
   return (
     <div>
-        
-        <iframe title="Benchmark AdServer Secom 2023" width="75%" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiY2JmYWU0M2UtMmYzZi00NGFjLWIyMGUtY2FkNTYzMjMyNGUwIiwidCI6ImExMTIwMGVkLTNhYTctNDFhMy05M2UxLTcwYWU4ZmMxZWMxYSJ9" frameborder="0" allowFullScreen="true"></iframe>
 
+        <iframe title="Campanhas Finalizadas 2023 - AdServer" width="75%" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiZmIxMDQxNzUtZmJkOC00ODI4LWIzZWUtMTdhZWZiMGRhOTM5IiwidCI6IjFjYzNjNTA4LTAxYzctNDQ2MC1iZDJiLWFmZTk1ZTgwYjhhZiJ9" frameborder="0" allowFullScreen="true"></iframe>
+       
     </div>
   )
 }
